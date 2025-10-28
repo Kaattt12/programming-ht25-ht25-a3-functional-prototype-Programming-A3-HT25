3162b054647aaf38ecab26899b41ca18edd1fa3d
Kaattt12 <231870780+Kaattt12@users.noreply.github.com>
2025-10-28 16:21:19 +0100

    added the bar, and the yellow death thingies have to change the function and add arrays and objects and shit

--- Template/script.js
+++ Template/script.js
@@ -6,8 +6,8 @@
 import * as Util from "./util.js";
 let Big=Util.createThing("moon");//tower
 let Mini=Util.createThing("sun");//pellets
-
-
+let Star =Util.createThing("star");//star
+let line = Util.createThing("line");//line
 
 const Sun ={//object of the sun/ aka the bunker
   hue:122,
@@ -19,8 +19,13 @@ const Sun ={//object of the sun/ aka the bunker
 const Moon ={
 hue:20,
 };
+const star ={
+hue:60,
+};
+const lineofdeath ={
+hue:90,
 
-
+}
 
 
 function bigSun(){  
@@ -30,18 +35,56 @@ Util.setPositionPixels(50,100,Mini )
 Util.setSize(30,30,Mini);
 Util.setColour(Moon.hue,Sun.saturation,Sun.lightness,Sun.alpha,Mini);//using the differnt hue but the same sat light and alpha as the sun
 
+  
 // Big Sun aka the bunker
 Util.setColour(Sun.hue,Sun.saturation,Sun.lightness,Sun.alpha,Big);
 Util.setRoundedness(0,Big);
-Util.setPositionPixels(100,100,Big);
+Util.setPositionPixels(50,500,Big);
 
-
-
+// Star enemeies 
+Util.setColour(star.hue,Sun.saturation,Sun.lightness,Sun.alpha,Star);
+Util.setSize(89,10,Star);
 
 }
 bigSun();
 
 
+// line which represents the line of death the yellow stuff shouldn't c
+function lineOfDeath(){
+Util.setColour(lineofdeath.hue,50,50,1,line)
+Util.setSize(800,10,line);
+Util.setPositionPixels(0,400,line);
+Util.setRoundedness(0,line)
+};
+
+lineOfDeath();
+// ...existing code...
+
+
+
+
+function handleKeyPress(event) {
+    console.log("Key pressed:", event.key);
+    // Add your key handling logic here
+
+    }
+
+
+// Remove the old Keypresessed function and replace with:
+document.addEventListener("keydown", handleKeyPress);
+// Remove the old Keypresessed function and replace with:
+
+
+
+
+
+
+
+// function Keypresessed(){
+//  addEventListener("keydown",listener) 
+// console.log("keydown","the key has been pressed")
+// }
+// Keypresessed()
 
 
 // State variables are the parts of your program that change over time.
